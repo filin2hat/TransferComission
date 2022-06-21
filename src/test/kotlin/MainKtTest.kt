0a41f0a799f97cc1240d5adae7daa7a9a75bffa6
Dmitriy Biryulin AN-32 <filin2hat@yandex.ru>
2022-06-21 20:49:40 +0300

    fix error to test calculateCommissionVkPayNoCommissionMonthLimit

--- src/test/kotlin/MainKtTest.kt
+++ src/test/kotlin/MainKtTest.kt
@@ -90,6 +90,6 @@ class MainKtTest {
         val transfer = 10_000_00
 
         val result = calculateCommission(cardType, previousPayments, transfer)
-        assertEquals(666, result)
+        assertEquals(0, result)
     }
 }
